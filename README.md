4115aa202910cf455758ea893da0ebffe2250686
jmanrocks152 <jmanrocks152@gmail.com>
2026-02-25 18:28:42 -0600

    Initial commit outlining interpreter work

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tis-100-webapp
-This repository is a web app port of TIS-100.
+This repository is a web app port of [TIS-100](https://store.steampowered.com/app/370360/TIS100/).
---
+++ src/Interpreter.ts
@@ -0,0 +1,18 @@
+type InputNodeState = {
+    data: number[],
+    dataPointer: number
+};
+
+type OutputNodeState = {
+    data: []
+};
+
+type ComputationNodeState = {
+    instructions: string[],
+    instructionPointer: number,
+    // TODO: Get the rest of the class attributes here
+}
+
+type NodeState = InputNodeState | OutputNodeState | ComputationNodeState;
+
+// TODO: Implement an interpreter class here, decide on data format for storing problems
