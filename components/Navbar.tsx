f25669818355eb57793c7e886e386f2f2ddb0336
jmanrocks152 <jmanrocks152@gmail.com>
2026-03-16 14:12:18 -0500

    Fixed dead link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestionCircle, faHome} from '@fortawesome/free-solid-svg-icons'
 
@@ -11,7 +13,7 @@ export default function Navbar({homeButtonHandler}: NavbarProps) {
         <div className="navbar flex items-center">
             <FontAwesomeIcon icon={faHome} size="2xl" onClick={homeButtonHandler}/>
             <h1 className="flex-1 font-title text-heading text-2xl font-bold text-center mt-2">TIS-100</h1>
-            <a href="/manual.pdf" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faQuestionCircle} size="2xl"/></a>
+            <Link href="/manual.pdf" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faQuestionCircle} size="2xl"/></Link>
         </div>
     );
 }
